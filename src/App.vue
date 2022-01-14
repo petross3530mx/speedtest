5658a9231f1da3808f8096388d312a3b85c0de38
Petro Melnyk <petross3530mx@gmail.com>
2022-01-14 03:11:18 +0200

    random numbers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Test />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Test,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fff;
+  background: #000;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="grid">
+    <div v-for="number of numbers" :key="number">{{ number }}</div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Test extends Vue {
+  numbers: number[] = [];
+
+  mounted(): void {
+    const numbers = [];
+    for (let i = 0; i < 25; i++) {
+      numbers[i] = i + 1;
+    }
+    this.numbers = numbers.sort(() => Math.random() - 0.5);
+  }
+}
+</script>
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(5, 3rem);
+  grid-template-rows: repeat(5, 3rem);
+  margin: auto;
+  max-width: 15rem;
+}
+.grid div {
+ display: flex;
+  border: 1px solid #fff;
+  align-items: center;
+  justify-content: center;
+}
+</style>
