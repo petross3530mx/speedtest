402cd47cbd49c6a047419ec8a4d58f271d235703
Petro Melnyk <petross3530mx@gmail.com>
2022-02-17 20:57:15 +0200

    intro screen added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
-    <Test />
+    <Test v-if="started" />
+    <div v-else>
+      <p>
+        Test is created to define a level of cognitive ability to work efficient
+      </p>
+      <p>You will need to find  numbers from 1 to 25 random positioned grid</p>
+      <p class="groupped centered">Results can be the next:
+        <ul>
+          <li>You are tired, lets sleep for 20 minutes to get working state </li>
+          <li>Maybe you need to relax before work, think about it</li>
+          <li>Go ahead, it's your the most efficient time, Don't spend that time for this test</li>
+        </ul>
+      </p>
+      <button @click="started = true">start</button>
+    </div>
   </div>
 </template>
 
@@ -13,10 +27,18 @@ import Test from "./components/Test.vue";
     Test,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  started = false;
+}
 </script>
 
 <style>
+.groupped.centered{
+  max-width: 450px;
+  text-align: left;
+  display: block;
+  margin: auto;
+}
 body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
